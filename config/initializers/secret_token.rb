b0ed7edc30ddc1cae301e04808c928b70310d911
scit106 <scit106@gmail.com>
2013-07-20 11:05:49 -0400

    initial commit for shakespeare project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shakes::Application.config.secret_token = 'a0a033f44ce747a6231c20ff7604f4cd4085fc2ae80e310327687eab3b40d8df15f6a5687124dfa039eed77e50a42b9eb409124e343b1c042949eeaf9f7b98e4'
