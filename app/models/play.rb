fe55f338b68dd3e771e881e2d316ee4a3bd1a415
scit106 <scit106@gmail.com>
2013-07-22 15:18:27 -0400

    added ability to parse out characters

--- app/models/play.rb
+++ app/models/play.rb
@@ -6,19 +6,29 @@ class Play < ActiveRecord::Base
 def self.play_parser
 	source = 'http://www.cafeconleche.org/examples/shakespeare/j_caesar.xml' #need to replace with variable
 	xml_string = RestClient.get(source)
-	parsed_string = Crack::XML.parse(xml_string)
+	@parsed_string = Crack::XML.parse(xml_string)
 
-	return parsed_string
+	return @parsed_string
 end
 
-# def self.play_title
-# 	name = play_parser.at_xpath("TITLE").text
-# end
+def self.play_title
+	@name = play_parser["PLAY"]["TITLE"]
 
-# def self.play_persona
-# 	character = play_parser.at_xpath("PERSONAE/*/PERSONA").each do |p|
-# 		p.text
-# 	end
-# end
+	return @name
+end
+
+#This returns the character names from the intro, but not the names used for lines
+def self.play_personae
+	#returns an array of non-grouped characters
+	@personae_main = play_parser["PLAY"]["PERSONAE"]["PERSONA"]
+	@personae_groups = play_parser["PLAY"]["PERSONAE"]["PGROUP"]
+
+	@personae = @personae_main
+	@personae_groups.each do |x|
+		@personae += x["PERSONA"]
+	end
+
+	return @personae
+end
 
 end
